290188e104274a322f52c73b7a1b52ee2504751d
Kyron Garnett <kyron_garnett@bloomfield.edu>
2016-10-18 21:08:37 -0400

    Midterm

---
+++ Midterm/Assets/Scripts/Hazard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hazard : MonoBehaviour {
+
+	public GameController GameControlr;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+		GameControlr.RespawnPlayer ();
+	}
+}
